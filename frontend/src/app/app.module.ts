84fc1bed9ab75d7ebdf3fd96a60d0cddfbb7ebc7
Trevor Bonomi <trevorbonomi@gmail.com>
2019-03-02 18:05:50 -0500

    markdown list

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,7 +1,7 @@
 //built in
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 //thirdparty
 
@@ -9,30 +9,22 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 //proprietary
 import { AppMaterialModule } from './app-material.module';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { SignUpComponent } from './user/sign-up/sign-up.component';
-import { AddressComponent } from './address/address.component';
-import { MatInputModule, MatButtonModule, MatSelectModule, MatRadioModule, MatCardModule } from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
-    SignUpComponent,
-    AddressComponent
+    SignUpComponent
   ],
   imports: [
     FormsModule,
     BrowserModule,
     AppRoutingModule,
-    AppMaterialModule,
-    MatInputModule,
-    MatButtonModule,
-    MatSelectModule,
-    MatRadioModule,
-    MatCardModule,
-    ReactiveFormsModule
+    AppMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
