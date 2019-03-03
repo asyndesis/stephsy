55839ac6e3dce1d5142c1798b18ee9b332e1975b
Trevor Bonomi <trevorbonomi@gmail.com>
2019-03-03 18:00:07 -0500

    database name

--- server/config/config.mjs
+++ server/config/config.mjs
@@ -1,12 +1,12 @@
 export default {
   development: {
     webServerPort : 3000,
-    mongoDatabaseUri : "mongodb://localhost:27017/moistdads",
+    mongoDatabaseUri : "mongodb://localhost:27017/stephsy",
     socketPort: 9999, /* update socket server file when you find out how to pass a socket */
   },
   production : {
     webServerPort : 80,
-    mongoDatabaseUri : "mongodb://xxxxx/DB_Name",
+    mongoDatabaseUri : "mongodb://xxxxx/stephsy",
     socketPort: 999
   }
 }   
